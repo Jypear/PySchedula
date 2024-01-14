a8a369b4dc9d2bfee24029ad04d8686df52f4569
Josh Pearson <jpear@JPear-Macbook.local>
2024-01-14 10:38:42 +0000

    Initial commit of project skeleton

---
+++ pyschedula/application.py
@@ -0,0 +1,15 @@
+class Schedula:
+    def run():
+        pass
+
+    async def serve():
+        pass
+
+    def play():
+        pass
+
+    def pause():
+        pass
+
+    def task(func):
+        pass
